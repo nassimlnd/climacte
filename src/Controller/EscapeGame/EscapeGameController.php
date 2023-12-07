c3c165955618f2feb9f1bc6cf7c68e1ecbe0f18a
YacineHB <95744524+YacineHB@users.noreply.github.com>
2023-12-08 00:17:37 +0100

    StartPage

---
+++ src/Controller/EscapeGame/EscapeGameController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller\EscapeGame;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+#[Route('/EscapeGame')]
+class EscapeGameController extends AbstractController
+{
+    #[Route('/', name: 'app_start')]
+    public function index(): Response
+    {
+        return $this->render('EscapeGame/Start.html.twig');
+    }
+
+}
