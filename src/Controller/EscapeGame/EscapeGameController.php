6d41421bc9216f91e105caca2583ae633da29ae2
YacineHB <95744524+YacineHB@users.noreply.github.com>
2023-12-08 02:07:56 +0100

    eg

--- src/Controller/EscapeGame/EscapeGameController.php
+++ src/Controller/EscapeGame/EscapeGameController.php
@@ -17,11 +17,34 @@ class EscapeGameController extends AbstractController
         return $this->render('EscapeGame/Start.html.twig');
     }
 
-    #[Route('/', name: 'app_start')]
-    public function index(): Response
+    #[Route('/entrer', name: 'app_entrer')]
+    public function entrer(): Response
     {
-        return $this->render('EscapeGame/Start.html.twig');
+        return $this->render('EscapeGame/Entrer.html.twig');
     }
 
+    #[Route('/jardin', name: 'app_jardin')]
+    public function jardin(): Response
+    {
+        return $this->render('EscapeGame/Jardin.html.twig');
+    }
+
+    #[Route('/maison', name: 'app_maison')]
+    public function maison(): Response
+    {
+        return $this->render('EscapeGame/Maison.html.twig');
+    }
+
+    #[Route('/rosier', name: 'app_rosier')]
+    public function rosier(): Response
+    {
+        return $this->render('EscapeGame/Rosier.html.twig');
+    }
+
+    #[Route('/cabane', name: 'app_cabane')]
+    public function cabane(): Response
+    {
+        return $this->render('EscapeGame/Cabane.html.twig');
+    }
 
 }
