66c1b8a5ca0b9bf6741703990a770b8b4f58ab4e
sir-martino <martin.neron@laposte.net>
2023-12-08 05:56:59 +0100

    pour nassim le best

---
+++ src/Controller/QuizController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class QuizController extends AbstractController
+{
+    #[Route('/quiz', name: 'app_quiz')]
+    public function index(): Response
+    {
+        return $this->render('quiz/index.html.twig', [
+            'controller_name' => 'QuizController',
+        ]);
+    }
+
+
+}
